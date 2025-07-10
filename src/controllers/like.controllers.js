c52c028c60c591b05ca6f34f8e447358315f481f
subhojitp7 <subhojit.paul@servicenow.com>
2025-07-11 01:43:25 +0530

    Completing like & tweet controllers

--- src/controllers/like.controllers.js
+++ src/controllers/like.controllers.js
@@ -7,20 +7,61 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 const toggleVideoLike = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
   //TODO: toggle like on video
+  const { userId } = req.user;
+  const like = await Like.findOne({ userId, videoId });
+  if (like) {
+    await like.deleteOne();
+    return res
+      .status(200)
+      .json(new ApiResponse(200, "Video unliked successfully"));
+  }
+  const newLike = new Like({ userId, videoId });
+  await newLike.save();
+  return res.status(200).json(new ApiResponse(200, "Video liked successfully"));
 });
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
   const { commentId } = req.params;
   //TODO: toggle like on comment
+  const { userId } = req.user;
+  const like = await Like.findOne({ userId, commentId });
+  if (like) {
+    await like.deleteOne();
+    return res
+      .status(200)
+      .json(new ApiResponse(200, "Comment unliked successfully"));
+  }
+  const newLike = new Like({ userId, commentId });
+  await newLike.save();
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Comment liked successfully"));
 });
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
   const { tweetId } = req.params;
   //TODO: toggle like on tweet
+  const { userId } = req.user;
+  const like = await Like.findOne({ userId, tweetId });
+  if (like) {
+    await like.deleteOne();
+    return res
+      .status(200)
+      .json(new ApiResponse(200, "Tweet unliked successfully"));
+  }
+  const newLike = new Like({ userId, tweetId });
+  await newLike.save();
+  return res.status(200).json(new ApiResponse(200, "Tweet liked successfully"));
 });
 
 const getLikedVideos = asyncHandler(async (req, res) => {
   //TODO: get all liked videos
+  const { userId } = req.user;
+  const likes = await Like.find({ userId });
+  const videos = await Video.find({
+    _id: { $in: likes.map((like) => like.videoId) },
+  });
+  return res.status(200).json(new ApiResponse(200, "OK", videos));
 });
 
 export { toggleCommentLike, toggleTweetLike, toggleVideoLike, getLikedVideos };
--- src/controllers/tweet.controllers.js
+++ src/controllers/tweet.controllers.js
@@ -1,4 +1,4 @@
-import mongoose, { isValidObjectId } from "mongoose";
+import mongoose from "mongoose";
 import { Tweet } from "../models/tweet.model.js";
 import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
@@ -7,18 +7,57 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const createTweet = asyncHandler(async (req, res) => {
   //TODO: create tweet
+  const { content } = req.body;
+  const { userId } = req.user;
+  const user = await User.findById(userId);
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+  const tweet = new Tweet({
+    content,
+    userId,
+    userName: user.userName,
+    avatar: user.avatar,
+  });
+  await tweet.save();
+  return res
+    .status(201)
+    .json(new ApiResponse(201, "Tweet created successfully", tweet));
 });
 
 const getUserTweets = asyncHandler(async (req, res) => {
   // TODO: get user tweets
+  const { userId } = req.params;
+  const tweets = await Tweet.find({ userId }).sort({ createdAt: -1 });
+  return res.status(200).json(new ApiResponse(200, "OK", tweets));
 });
 
 const updateTweet = asyncHandler(async (req, res) => {
   //TODO: update tweet
+  const { tweetId } = req.params;
+  const { content } = req.body;
+  const tweet = await Tweet.findById(tweetId);
+  if (!tweet) {
+    throw new ApiError(404, "Tweet not found");
+  }
+  tweet.content = content;
+  await tweet.save();
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Tweet updated successfully", tweet));
 });
 
 const deleteTweet = asyncHandler(async (req, res) => {
   //TODO: delete tweet
+  const { tweetId } = req.params;
+  const tweet = await Tweet.findById(tweetId);
+  if (!tweet) {
+    throw new ApiError(404, "Tweet not found");
+  }
+  await tweet.deleteOne();
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Tweet deleted successfully"));
 });
 
 export { createTweet, getUserTweets, updateTweet, deleteTweet };
